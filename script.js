a1a674617d4ceb1b831e61411231e79b824e1b41
aaronRhodebeck <arhodebeck@gmail.com>
2017-02-14 13:35:33 -0600

    Moving to apartment

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-function testCheck() {
-    this.testAreWorking = true
-};
+function SearchArgs(searchText, whatToSearch, metadataToReturn, propertiesToReturn, pageToStartOn, totalPagesToReturn) {
+    this.searchText = searchText;
+    this.srwhat = whatToSearch;
+    this.srinfo = metadataToReturn;
+    this.srprop = propertiesToReturn;
+    this.sroffset = pageToStartOn;
+    this.srlimit = totalPagesToReturn;
+}
+
+function WikipediaSearch(searchArgs) {
+    this.searchArgs = searchArgs;
+    this.searchResults;
+
+    var createSearchString = function(searchText) {
+        return encodeURIComponent(searchText);
+    }
+
+    var createAPICall = function() {
+        var srsearch = createSearchString(this.searchArgs.searchText);
+        return "action=query&list=search&" +
+            "srsearch=" + srsearch + "&" +
+            "srwhat=" + searchArgs.srwhat + "&" +
+            "srinfo=" + searchArgs.srinfo + "&" +
+            "srprop=" + searchArgs.srprop + "&" +
+            "sroffset=" + searchArgs.sroffset + "&" +
+            "srlimit=" + searchArgs.srlimit;
+    }
+
+    this.apiCall = createAPICall();
+
+
+    this.searchWikipedia = function() {
+        var wikipediaURL = "https://en.wikipedia.org/w/api.php?";
+        $.getJSON(wikipediaURL + this.apiCall, function(value) {
+            this.searchResults = value;
+        });
+    }
+}
--- spec/scriptSpec.js
+++ spec/scriptSpec.js
@@ -1,7 +1,58 @@
-describe("Tests are working", function() {
-    var test = new testCheck();
+describe("SearchArgs", function() {
+    it("should take at least one but up to six values", function() {
+        var testArgs = new SearchArgs(
+            "Search Text",
+            "Search Area",
+            "Metadata Requested",
+            "Properties Requested",
+            "Starting Page",
+            "Total Pages"
+        );
 
-    it("should be true", function() {
-        expect(test.testAreWorking).toBe(true);
+        expect(testArgs.searchText).toBe("Search Text");
+        expect(testArgs.srwhat).toBe("Search Area");
+        expect(testArgs.srinfo).toBe("Metadata Requested");
+        expect(testArgs.srprop).toBe("Properties Requested");
+        expect(testArgs.sroffset).toBe("Starting Page");
+        expect(testArgs.srlimit).toBe("Total Pages");
     });
+})
+
+describe("WikipediaSearch", function() {
+    var searchArgs = new SearchArgs(
+        "wikipedia search",
+        "title",
+        "suggestion",
+        "snippet|hasrelated",
+        "0",
+        "1"
+    );
+    var testSearch = new WikipediaSearch(searchArgs);
+
+    it("it's constructor should accept an object, and should make it a public property", function() {
+        expect(searchArgs.testSearch).toBe("wikipedia search");
+        expect(testSearch.searchArgs).toEqual(searchArgs);
+    });
+
+    it("should create the API call string", function() {
+        var apiCall = testSearch.apiCall();
+        expect(apiCall).toEqual(
+            "action=query&list=search&" +
+            "srsearch=wikipedia%20search&" +
+            "srwhat=title&" +
+            "srinfo=suggestion&" +
+            "srprop=snippet|hasrelated&" +
+            "sroffset=0&" +
+            "srlimit=1"
+        );
+    });
+
+    it("should call the wikipedia search API", function() {
+        spyOn($, "getJSON");
+        this.testSearch.searchWikipedia();
+
+        expect($.getJSON).toHaveBeenCalled();
+        expect($.getJSON).toHaveBeenCalledWith("https://en.wikipedia.org/w/api.php?");
+    });
+
 });
